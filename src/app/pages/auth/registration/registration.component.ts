b6d151301f69eebbc5b8b3346cb721205e3e7b44
Ala <alla.dubowikowa@yandex.ru>
2025-03-15 10:24:31 +0300

    add Form for registration.component

--- src/app/pages/auth/registration/registration.component.ts
+++ src/app/pages/auth/registration/registration.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [InputTextModule, CheckboxModule, ButtonModule, FormsModule, NgClass],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
-export class RegistrationComponent { }
+export class RegistrationComponent implements OnInit {
+
+  login: string;
+  password: string;
+  repeatPassword: string;
+  cardNumber: string;
+  email: string;
+  isRemember: boolean;
+  labelText = "Сохранить пользователя в хранилище";
+  constructor(private userService: UserService) {
+
+  }
+ngOnInit(): void {
+  
+}
+
+onAuth(ev: Event): void {
+  console.log(ev, 'ev');
+  this.userService.addUser({login: this.login, password: this.password});
+}
+
+}
